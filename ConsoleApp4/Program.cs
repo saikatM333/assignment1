4182fb6a740edc3e08adbf7f5d3449c33b707a5e
saikatCV <saikat030703@gmail.com>
2024-03-14 11:00:45 +0530

    progrmaing question on finding the grade using conditional statement I have used switch

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿ class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Marks predictor ");
+
+        int marks = Convert.ToInt32(Console.ReadLine());
+        marks = marks / 10;
+
+        switch (marks)
+        {
+            case 10:
+            case 9: Console.WriteLine("Your grade is A");
+                break;
+
+            case 8: Console.WriteLine("your grade is B");
+                break;
+            case 7: Console.WriteLine("your grade is C");
+                break;
+            case 6: Console.WriteLine("your grade is D");
+                break;
+            default: Console.WriteLine("your grade is F");
+                break ;
+        }
+
+
+    }
+}
